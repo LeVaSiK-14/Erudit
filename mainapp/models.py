31dc5ac5aac2f23d9d07a247a709a90ba2e364cc
leva-14 <lev201611@gmail.com>
2021-10-22 11:12:26 +0600

    Add models Teacher, Subjects

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,2 +1,22 @@
 from django.db import models
 
+class Teacher(models.Model):
+
+    first_name = models.CharField(max_length=127, verbose_name='Имя учителя')
+    last_name = models.CharField(max_length=127, verbose_name='Фамилия учителя', null=True)
+    patronymic = models.CharField(max_length=127, verbose_name='Отчество учителя', null=True)
+
+    about = models.TextField(verbose_name='О преподователе')
+    image = models.ImageField(upload_to='media/mainapp/images', null=True, blank=True, verbose_name='Фото учителя')
+
+    def __str_(self):
+        return f'{self.first_name} - {self.patronymic}'
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=127, verbose_name='Название предмета')
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, verbose_name='Учитель курса')
+    duration = models.PositiveIntegerField(default=0)
+    about = models.TextField()
+    
+    def __str__(self):
+        return f'{self.teacher.first_name} - {self.subject_name}'
